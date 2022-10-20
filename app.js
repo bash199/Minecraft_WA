b2689e9f93ad598e018fe64259a152593941d727
Bashir Tarif <112946825+bash199@users.noreply.github.com>
2022-10-21 01:50:47 +0300

    imgs added & files modified

--- app.js
+++ app.js
@@ -1,29 +1,74 @@
+const container = document.querySelector('.Minecraft-container');
 
-const container = document.querySelector('.container');
+const game = document.querySelector('.tools-container');
 
-// document.createElement('div')
-
-
-
-
-const q=container.appendChild(document.createElement('div'))
-q.setAttribute('class','red')
-container.appendChild(document.createElement('div'))
-container.appendChild(document.createElement('div'))
-container.appendChild(document.createElement('div'))
-container.appendChild(document.createElement('div'))
-container.appendChild(document.createElement('div'))
-container.appendChild(document.createElement('div'))
-container.appendChild(document.createElement('div'))
-container.appendChild(document.createElement('div'))
 let inventory ={
+   rowTemp: 16,
+   colTemp: 16,
    current: '',
+   tree: 'tree',
+   rock: 'rock',
+   soil: 'soil',
+   sky: '',
+   cloud: 'cloud', 
+   grass:'grass',
+   leaves:'leaves'
 }
+intializeGame()
+function intializeGame(){
+   for(let i = 0;i< inventory.rowTemp;i++){
+      for(let k = 0;k< inventory.colTemp;k++){
+         // container.style.display='grid';
+         // container.style.gridTemplate=`repeat(${inventory.rowTemp},1fr) / repeat(${inventory.colTemp},1fr)`
+         let div = document.createElement('div');
+         div.setAttribute('id',`cell${i}-${k}`)
+         div.setAttribute('class','cell')
+         // div.innerHTML = '';
+         container.appendChild(div)
+      }
+   } 
+}
+function fillCells(){
+   for(let k = 0;k< container.children.length;k++){
+      let [i,j] = container.children[k].getAttribute('id').slice(4).split('-');
+      
+      
+       fillGrass(i,j,k)
+       filltree(i,j,k)
+       fillLeaves(i,j,k)
+   } 
+
+}
+function fillLeaves(i,j,k){
+   if( i<8 && i>5 && j>7 && j<11 ){
+      container.children[k].classList.add(inventory.leaves);
+   }
+}
+
+function filltree(i,j,k){
+   if((i==8||i==11||i==10||i==9)&&j==9){
+      container.children[k].classList.add(inventory.tree);
+   }
+   
+}
+
+function fillGrass(i,j,k){
+   if(i==12){
+      container.children[k].classList.add(inventory.grass);
+   }
+   else if(i>=13){
+      container.children[k].classList.add(inventory.soil);
+   }
+}
+
+
+
+
+fillCells()
+
+
 container.addEventListener('click',(event)=>{
-   let clas = event.target;
-   takesTheClass(clas)
+   if(!event.target.getAttribute('class').includes('tree')){
+      event.target.setAttribute('class',inventory.tree);
+   }
 })
-function takesTheClass(element){
-   console.log();
-   inventory.current=element.getAttribute('class');
-}
